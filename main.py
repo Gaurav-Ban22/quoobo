d6dfd1306daa77c443769ecec23305d2194bd859
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-08-17 22:42:21 -0700

    programminggit add .!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import argparse
 
 parser = argparse.ArgumentParser();
-subparser = parser.add_subparsers(dest='com')
+subparser = parser.add_subparsers(dest='com', help="sub-command-help")
 
-about = subparser.add_parser("about")
-about.add_argument("about", help="about quoobo", action="store_true")
+about = subparser.add_parser("about", help="about command")
+about.add_argument("--m", help="more about information", action="store_true")
 args = parser.parse_args()
 
 if args.com == "about":
-    print("Quoobo is a program to make unix-based-commands a little bit more fun. It was made in python by Gaurav-Ban22. Check him out on github!")
+    if (not args.m):
+        print("Quoobo is a program to make unix-based-commands a little bit more fun. It was made in python by Gaurav-Ban22. Check him out on github!")
+    else:
+        print("Quoobo is a program to make unix-based-commands a little bit more fun. It was made in python by Gaurav-Ban22. Check him out on github!\nThere are also some other things you might want to see; check -h for more commands!")
