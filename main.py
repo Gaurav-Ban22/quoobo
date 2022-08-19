796fc9d13a8713ea9493d0445ab8c5cf75693612
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-08-18 18:50:14 -0700

    fixed multiargument

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ subparser = parser.add_subparsers(dest='com', help="sub-command-help")
 
 about = subparser.add_parser("about", help="about command")
 about.add_argument("-m", help="more about information", action="store_true")
-echo = subparser.add_parser("echo", help = "repeat any string")
-repeated = echo.add_argument("echoo")
+echo = subparser.add_parser("echo", help = "repeat any singularworded string")
+repeated = echo.add_argument("echoo", nargs="+")
 args = parser.parse_args()
 
 if args.com == "about":
@@ -15,4 +15,8 @@ if args.com == "about":
     else:
         print("Quoobo is a program to make unix-based-commands a little bit more fun. It was made in python by Gaurav-Ban22. Check him out on github!\nThere are also some other things you might want to see; check -h for more commands!")
 elif args.com == "echo":
-    print(args.echoo)
+    x = ""
+    for i in enumerate(args.echoo):
+        x += i[1] + ""
+
+    print(x)
