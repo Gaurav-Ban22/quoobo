ab27da84336d827e5aff982594310c4cc5269979
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-08-20 18:39:00 -0700

    UNSTABLE AND EDXPANDED VERSIONS OF LS WORK

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import argparse
 from ctypes.wintypes import tagMSG
 
+
 from os import *
 
+RESET = "\u001B[0m"
+BLACK = "\u001B[30m"
+RED = "\u001B[31m"
+GREEN = "\u001B[32m"
+YELLOW = "\u001B[33m"
+BLUE = "\u001B[34m"
+PURPLE = "\u001B[35m"
+CYAN = "\u001B[36m"
+WHITE = "\u001B[37m"
+BLACK_BOLD = "\033[1;30m"
+RED_BOLD = "\033[1;31m"
+GREEN_BOLD = "\033[1;32m"
+YELLOW_BOLD = "\033[1;33m"
+BLUE_BOLD = "\033[1;34m"
+PURPLE_BOLD = "\033[1;35m"
+CYAN_BOLD = "\033[1;36m"
+WHITE_BOLD = "\033[1;37m"
+
+def colorize(text, color):
+    return color + text + RESET
 
 class algorithm:
 
@@ -20,7 +41,9 @@ about = subparser.add_parser("about", help="about command")
 about.add_argument("-m", help="more about information", action="store_true")
 echo = subparser.add_parser("echo", help = "repeat any singularworded string")
 repeated = echo.add_argument("echoo", nargs="+")
-list = subparser.add_parser("ls", help = "simple lister")
+list = subparser.add_parser("ls", help = "blue is file, green is folder")
+repeato = list.add_argument("-u", help="unstable expanded format", action="store_true")
+repeato = list.add_argument("-e", help="expanded format", action="store_true")
 args = parser.parse_args()
 
 if args.com == "about":
@@ -34,7 +57,43 @@ elif args.com == "echo":
         x += i[1] + " "
         print(x)
 elif args.com == "ls":
-    fileName = [f for f in listdir(getcwd())]
-    print(fileName)
+    if (not args.e and not args.u):
+        for f in listdir(getcwd()):
+            
+            if (path.isdir(getcwd()+"/"+f)):
+                if (f.startswith(".")):
+                    print(colorize(f, YELLOW))
+                print(colorize(f, GREEN))
+            else:
+                print(colorize(f, BLUE))
+    elif (args.u):
+        for f in listdir(getcwd()):
+            
+            if (path.isdir(getcwd()+"/"+f)):
+                print(colorize(f, GREEN))
+                if (f.startswith(".")):
+                    print(colorize(f, YELLOW))
+                for f in listdir(getcwd()+"/"+f):
+                    print("  -" + colorize(f, RED))
+            else:
+                print(colorize(f, BLUE))
+
+    elif(args.e):
+        for f in listdir(getcwd()):
+            
+            if (path.isdir(getcwd()+"/"+f)):
+                if (not f.startswith(".")):
+                    print(colorize(f, GREEN))
+                    for f in listdir(getcwd()+"/"+f):
+                        print("  -" + colorize(f, RED))
+                else:
+                    print(colorize(f, YELLOW))
+            else:
+                print(colorize(f, BLUE))
+
+
+        
+        
+        
 
     
